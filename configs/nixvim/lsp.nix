982a23df005d7c27cbb0a56254581fa1c15e141b
Isaac Hayes <me@isaachayes.au>
2025-02-13 16:39:39 +1100

    Fix Tabout

--- configs/nixvim/lsp.nix
+++ configs/nixvim/lsp.nix
@@ -50,20 +50,39 @@
 
     # Ability to toggle cmp
     extraConfigLua = ''
+                  local format_enabled = true
+                    vim.api.nvim_create_user_command("ToggleFormatNotified", function()
+              if format_enabled then
+             vim.cmd("FormatDisable")
+                           require("notify")("Disabled formatting")
+                   		format_enabled = false
+                   	else
+             vim.cmd("FormatEnable")
+                           require("notify")("Enabled formatting")
+                   		format_enabled = true
+                    	end
+                   end, {})
 
-           local format_enabled = true
-             vim.api.nvim_create_user_command("ToggleFormatNotified", function()
-       if format_enabled then
-      vim.cmd("FormatDisable")
-                    require("notify")("Disabled formatting")
-            		format_enabled = false
-            	else
-      vim.cmd("FormatEnable")
-                    require("notify")("Enabled formatting")
-            		format_enabled = true
-             	end
-            end, {})
-
+         require('tabout').setup {
+        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        act_as_tab = true, -- shift content if tab out is not possible
+        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<C-d>', -- reverse shift default action,
+        enable_backwards = true, -- well ...
+        completion = true, -- if the tabkey is used in a completion pum
+        tabouts = {
+          {open = "'", close = "'"},
+          {open = '"', close = '"'},
+          {open = '`', close = '`'},
+          {open = '(', close = ')'},
+          {open = '[', close = ']'},
+          {open = '{', close = '}'}
+        },
+        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+        exclude = {} -- tabout will ignore these filetypes
+      }
     '';
     keymaps = [
       {
