d5b6ff6b4089f85c0d85bc8e31af7beaadf1d961
Isaac Hayes <me@isaachayes.au>
2025-02-18 14:47:46 +1100

    Kitty colors update

--- configs/kitty.nix
+++ configs/kitty.nix
@@ -6,44 +6,62 @@
     enable = true;
     shellIntegration.enableZshIntegration = true;
     settings = {
-	  opacity = 0.8;
+      opacity = 0.8;
+
       # The basic colors
-      foreground = "#89b4fa";
-      background = "#1E1E2E";
-      selection_foreground = "#1E1E2E";
-      selection_background = "#89b4fa";
+      foreground = "#979eab";
+      background = "#282c34";
+      selection_foreground = "#282c34";
+      selection_background = "#979eab";
 
       # Cursor colors
-      cursor = "#89b4fa";
-      cursor_text_color = "#89b4fa";
+      cursor = "#979eab";
+      cursor_text_color = "#282c34";
 
       # URL underline color when hovering with mouse
-      url_color = "#89b4fa";
+      url_color = "#61afef";
 
       # Kitty window border colors
-      active_border_color = "#89b4fa";
-      inactive_border_color = "#89b4fa";
-      bell_border_color = "#89b4fa";
+      active_border_color = "#61afef";
+      inactive_border_color = "#393e48";
+      bell_border_color = "#e06c75";
 
       # OS Window titlebar colors
       wayland_titlebar_color = "system";
       macos_titlebar_color = "system";
 
       # Tab bar colors
-      active_tab_foreground = "#89b4fa";
-      active_tab_background = "#89b4fa";
-      inactive_tab_foreground = "#89b4fa";
-      inactive_tab_background = "#89b4fa";
-      tab_bar_background = "#89b4fa";
+      active_tab_foreground = "#282c34";
+      active_tab_background = "#979eab";
+      inactive_tab_foreground = "#abb2bf";
+      inactive_tab_background = "#282c34";
+      tab_bar_background = "#282c34";
 
       # Colors for marks (marked text in the terminal)
-      mark1_foreground = "#a6e3a1";
-      mark1_background = "#89b4fa";
-      mark2_foreground = "#a6e3a1";
-      mark2_background = "#89b4fa";
-      mark3_foreground = "#a6e3a1";
-      mark3_background = "#89b4fa";
+      mark1_foreground = "#98c379";
+      mark1_background = "#282c34";
+      mark2_foreground = "#e5c07b";
+      mark2_background = "#282c34";
+      mark3_foreground = "#61afef";
+      mark3_background = "#282c34";
 
+      # Extended colors
+      color0 = "#282c34";
+      color1 = "#e06c75";
+      color2 = "#98c379";
+      color3 = "#e5c07b";
+      color4 = "#61afef";
+      color5 = "#be5046";
+      color6 = "#56b6c2";
+      color7 = "#979eab";
+      color8 = "#393e48";
+      color9 = "#d19a66";
+      color10 = "#56b6c2";
+      color11 = "#e5c07b";
+      color12 = "#61afef";
+      color13 = "#be5046";
+      color14 = "#56b6c2";
+      color15 = "#abb2bf";
     };
   };
 }
